9f3419cfc7df7db78d0cfc240e1ac74d928cfa9d
Kalystee <34235752+Kalystee@users.noreply.github.com>
2025-05-30 22:23:54 -0400

    feat(icons) :art: add icons (#1)
    * feat(icons) :art: add icons
    
    * chore(ga) Rename build name

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -1,11 +1,16 @@
 <script setup>
 
+import BackIcon from "@/components/icons/BackIcon.vue";
 </script>
 
 <template>
 <div class="container m-auto p-6">
-  <RouterLink to="/players" class="text-green-500 hover:text-green-800">
-    Back to Players
-  </RouterLink>
+  <div class="flex items-center">
+    <BackIcon/>
+    <RouterLink to="/players" class="text-green-500 hover:text-green-800 ms-1">
+      Back to Players
+    </RouterLink>
+  </div>
+
 </div>
 </template>
